fb9e6f9d3bc22772780633331a0f4cf0c9f7225d
aloisiopf <aloisiopf@gmail.com>
2022-07-20 19:28:22 -0300

    Criação da solução para o desafio 03 (Fatorial Simples)

---
+++ desafio03-fatorialsimples.js
@@ -0,0 +1,15 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída(output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+var N = parseInt(gets());
+var fatorial = 1;
+
+if (this.N>0){
+  fatorial = N;
+  while (N > 1){
+    fatorial = fatorial * (N-1);
+    N--;
+  }
+}
+
+print(this.fatorial);
